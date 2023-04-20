2484dfe5cdd4fd09f46394a285c54a6ac4b0a06d
Abhay S Nath <abhaysnath502@gmail.com>
2023-04-20 21:06:20 +0530

    Update App.js

--- coding-exercise/src/App.js
+++ coding-exercise/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload browser.
         </p>
         <a
           className="App-link"
